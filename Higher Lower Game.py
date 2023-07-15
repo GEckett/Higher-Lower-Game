8ecf8482ef59604f96274fdbb3f5ac9386a5ced6
GEckett <grant.eckett@gmail.com>
2023-07-15 15:54:11 +0100

    Update Higher Lower Game.py

--- Higher Lower Game.py
+++ Higher Lower Game.py
@@ -1,14 +1,3 @@
-#To Do
-
-#logo should remain on console while game is running
-#while loop that continues while user guesses correctly - yes
-#Random selection of first pair - yes
-#A description of both people with thier Name,Job and where they live shoud appear after first pair is selected - yes
-#Need to keep track of score, adding to it as correct guesses are made - yes
-#On correct guesses, B=A and a new random person should be added to B
-#Game should end if incorrect answer is given and show score - yes
-
-
 #Imports
 import os
 import random
@@ -39,25 +28,22 @@ def game():
             Choice = a
             if Choice['follower_count'] > b['follower_count']:
                 points += 1
-                b=a
+                a=b
                 b=random_B()
                 os.system('cls||clear')
             else:
                 os.system('cls||clear')
-                print(logo)
                 print(f"Sorry that's wrong.Your final score is: {points}")
                 end = True  
         elif Choice == "b":
             Choice = b
             if Choice['follower_count'] > a['follower_count']:
                 points += 1
-                b=a
+                a=b
                 b=random_B()
                 os.system('cls||clear')
-                print(logo)
             else:
                 os.system('cls||clear')
-                print(logo)
                 print(f"Sorry that's wrong.Your final score is: {points}")
                 end = True  
 
@@ -65,4 +51,5 @@ def game():
 print(logo)
 while input("Do you want to play Higher Lower? y(yes) or n(no): ") == "y":
     os.system('cls||clear')
+    print (logo)
     game()
